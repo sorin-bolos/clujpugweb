eb50bc45574b91cfb71d075030fcd3095004f49f
sorin-bolos <sorinalbolos@yahoo.com>
2026-02-03 16:28:57 +0200

    Add ezoic ads

--- ClujPugWeb/Program.cs
+++ ClujPugWeb/Program.cs
@@ -2,6 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
+// Redirect ads.txt to Ezoic's managed ads.txt
+app.MapGet("/ads.txt", () => Results.Redirect("https://srv.adstxtmanager.com/19390/clujpug.ro", permanent: true));
+
 // Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
